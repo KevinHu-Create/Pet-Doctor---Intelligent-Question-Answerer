f9b65430ec2852c9d61c252299cf23ac559fc92d
KevinHu-Create <hu.3116@buckeyemail.osu.edu>
2026-03-22 01:46:42 -0400

    Added gitnore modified settings and main
    - gitnore for not submit pycache, local db and env
    - main added 2 new routes
    - change settings into standard Pydantic Basesettings writing format

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,18 +1,19 @@
 import os
 from pathlib import Path
+from pydantic_settings import BaseSettings
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
-class Settings:
-    DATA_DIR = os.getenv("DATA_DIR", PROJECT_ROOT / "data")
-    CHUNK_SIZE = int(os.getenv("CHUNK_SIZE", "800"))
-    CHUNK_OVERLAP = int(os.getenv("CHUNK_OVERLAP", "120"))
+class Settings(BaseSettings):
+    database_url: str = "sqlite:///./users.db"
+    DATA_DIR: Path = os.getenv("DATA_DIR", PROJECT_ROOT / "data")
+    CHUNK_SIZE: int = int(os.getenv("CHUNK_SIZE", "800"))
+    CHUNK_OVERLAP: int = int(os.getenv("CHUNK_OVERLAP", "120"))
 
-    HF_EMBED_MODEL = os.getenv("HF_EMBED_MODEL", "sentence-transformers/all-MiniLM-L6-v2")
-    OLLAMA_CHAT_MODEL = os.getenv("OLLAMA_CHAT_MODEL", "llama3.2")
-    OLLAMA_BASE_URL = os.getenv("OLLAMA_BASE_URL", "http://ollama:11434")
-
-    MILVUS_URI = os.getenv("MILVUS_URI", "http://milvus:19530")
-    COLLECTION_NAME = os.getenv("COLLECTION_NAME", "wiki_insulin_st_minilm_v1")
+    HF_EMBED_MODEL: str = os.getenv("HF_EMBED_MODEL", "sentence-transformers/all-MiniLM-L6-v2")
+    OLLAMA_CHAT_MODEL: str = os.getenv("OLLAMA_CHAT_MODEL", "llama3.2")
+    OLLAMA_BASE_URL: str = os.getenv("OLLAMA_BASE_URL", "http://ollama:11434")
+    MILVUS_URI: str = os.getenv("MILVUS_URI", "http://milvus:19530")
+    COLLECTION_NAME: str = os.getenv("COLLECTION_NAME", "wiki_insulin_st_minilm_v1")
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from app.routes.health import router as health_router
 from app.routes.qa import router as qa_router
+from app.routes.auth import router as auth_router
+from app.routes.user import router as user_router
+from app.db.database import engine
+from app.db.models import Base
 
+
+Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Pet Doctor RAG API", version="1.0")
 
 app.include_router(health_router)
 app.include_router(qa_router)
+app.include_router(auth_router)
+app.include_router(user_router)
